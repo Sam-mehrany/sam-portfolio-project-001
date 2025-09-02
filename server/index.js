a5cbb79df5542bf8ad475f86d2d1bd16936ab668
Sam-mehrany <sammehrany@gmail.com>
2025-09-02 14:29:03 +0330

    Update index.js

--- server/index.js
+++ server/index.js
@@ -26,7 +26,7 @@ app.use((req, res, next) => {
 
 // --- Multer Setup for File Uploads ---
 const uploadsDir = '/uploads';
-if (!fs.existsSync(uploadsDir)) fs.mkdirSync(uploadsDir);
+
 const storage = multer.diskStorage({
   destination: (req, file, cb) => cb(null, 'uploads/'),
   filename: (req, file, cb) => {
